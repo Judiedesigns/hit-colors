ef03affa7fdbca7d0a50b27f4740199826ec4bf0
Florence Eze <florencekey22@gmail.com>
2026-08-11 01:43:59 -0700

    Fix Vercel build and update title font

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,14 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'index.html',
-        demos: 'demos/index.html',
-        text: 'demos/text/index.html',
-        matrix: 'demos/matrix/index.html',
-      },
-    },
-  },
 });
